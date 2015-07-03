8320d2b471298469054c8153fe9ca75e5f811d57
Joe Levy <jolevy@JOLEVY-LENOVO.redmond.corp.microsoft.com>
2015-07-02 18:40:05 -0700

    refactored based on this morning discussion

---
+++ AutomationISE/Model/AutomationAssetManager.cs
@@ -0,0 +1,87 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Azure.Management.Automation;
+using Microsoft.Azure.Management.Automation.Models;
+
+namespace AutomationISE.Model
+{
+    public class AutomationAssetManager 
+    {
+        public static async void DownloadAllFromCloud(String localWorkspacePath, AutomationManagementClient automationApi, string resourceGroupName, string automationAccountName)
+        {
+            var assets = await AutomationAssetManager.GetAll(null, automationApi, resourceGroupName, automationAccountName);
+            AutomationAssetManager.SaveLocally(localWorkspacePath, assets);
+        }
+
+        public static void SaveLocally(String localWorkspacePath, ISet<AutomationAsset> assets)
+        {
+            LocalAssetsStore.Set(localWorkspacePath, assets);
+        }
+
+        public static async Task<ISet<AutomationAsset>> GetAll(String localWorkspacePath, AutomationManagementClient automationApi, string resourceGroupName, string automationAccountName)
+        {
+            VariableListResponse cloudVariables = await automationApi.Variables.ListAsync(resourceGroupName, automationAccountName);
+            CredentialListResponse cloudCredentials = await automationApi.PsCredentials.ListAsync(resourceGroupName, automationAccountName);
+
+            LocalAssets localAssets = LocalAssetsStore.Get(localWorkspacePath);
+
+            var automationAssets = new SortedSet<AutomationAsset>();
+
+            // Compare cloud variables to local
+            foreach (var cloudAsset in cloudVariables.Variables)
+            {
+                var localAsset = localAssets.Variables.Find(asset => asset.Name == cloudAsset.Name);
+
+                var automationAsset = (localAsset != null) ?
+                        new AutomationVariable(localAsset, cloudAsset) :
+                        new AutomationVariable(cloudAsset);
+
+                automationAssets.Add(automationAsset);
+            }
+
+            // Add remaining locally created variables
+            foreach (var localAsset in localAssets.Variables)
+            {
+                var automationAsset = new AutomationVariable(localAsset);
+                automationAssets.Add(automationAsset);
+            }
+
+            // Compare cloud credentials to local
+            foreach (var cloudAsset in cloudCredentials.Credentials)
+            {
+                var localAsset = localAssets.PSCredentials.Find(asset => asset.Name == cloudAsset.Name);
+
+                var automationAsset = (localAsset != null) ?
+                        new AutomationCredential(localAsset, cloudAsset) :
+                        new AutomationCredential(cloudAsset);
+
+                automationAssets.Add(automationAsset);
+            }
+
+            // Add remaining locally created credentials
+            foreach (var localAsset in localAssets.PSCredentials)
+            {
+                var automationAsset = new AutomationCredential(localAsset);
+                automationAssets.Add(automationAsset);
+            }
+
+            return automationAssets;
+        }
+    }
+}
---
+++ AutomationISE/Model/LocalAssetsStore.cs
@@ -0,0 +1,185 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.Azure.Management.Automation;
+using System.Web.Script.Serialization;
+using Newtonsoft.Json;
+
+namespace AutomationISE.Model
+{
+    public class LocalAssetsStore
+    {
+        public static void Set(String workspacePath, ISet<AutomationAsset> newAssets)
+        {
+            LocalAssets localAssets = LocalAssetsStore.Get(workspacePath);
+
+            // add / update variables
+            foreach (var newAsset in newAssets)
+            {
+                if (!(newAsset is AutomationVariable))
+                {
+                    continue;
+                }
+                
+                bool found = false;
+                foreach (var currentLocalAsset in localAssets.Variables)
+                {
+                    if (newAsset.Name == currentLocalAsset.Name)
+                    {
+                        found = true;
+                        currentLocalAsset.Update(newAsset);
+                    }
+                }
+
+                if (!found)
+                {
+                    localAssets.Variables.Add(new VariableJson((AutomationVariable)newAsset));
+                }
+            }
+
+            // add / update credentials
+            foreach (var newAsset in newAssets)
+            {
+                if (!(newAsset is AutomationCredential))
+                {
+                    continue;
+                }
+                
+                bool found = false;
+                foreach (var currentLocalAsset in localAssets.PSCredentials)
+                {
+                    if (newAsset.Name == currentLocalAsset.Name)
+                    {
+                        found = true;
+                        currentLocalAsset.Update(newAsset);
+                    }
+                }
+
+                if (!found)
+                {
+                    localAssets.PSCredentials.Add(new CredentialJson((AutomationCredential)newAsset));
+                }
+            }
+
+            DirectoryInfo dir = Directory.CreateDirectory(workspacePath);
+            UnsecureLocalAssetsContainerJson.Set(workspacePath, localAssets);
+            SecureLocalAssetsContainerJson.Set(workspacePath, localAssets); 
+        }
+        
+        public static LocalAssets Get(String workspacePath)
+        {
+            LocalAssets localAssetsContainer = new LocalAssets(); 
+            
+            UnsecureLocalAssetsContainerJson localAssetsJson = UnsecureLocalAssetsContainerJson.Get(workspacePath);
+            SecureLocalAssetsContainerJson secureLocalAssetsJson = SecureLocalAssetsContainerJson.Get(workspacePath);
+            
+            // add JSON variables to the container
+            localAssetsJson.Variable.ForEach(variable => variable.Encrypted = false);
+            localAssetsContainer.Variables.AddRange(localAssetsJson.Variable);
+
+            secureLocalAssetsJson.Variable.ForEach(variable => variable.Encrypted = true);
+            localAssetsContainer.Variables.AddRange(secureLocalAssetsJson.Variable);
+
+            // add JSON credentials to the container
+            localAssetsContainer.PSCredentials.AddRange(secureLocalAssetsJson.PSCredential);
+
+            return localAssetsContainer;
+        }
+
+        private abstract class AbstractLocalAssetsContainerJson
+        {
+            public List<VariableJson> Variable = new List<VariableJson>();
+            public static JavaScriptSerializer jss = new JavaScriptSerializer();
+
+            public static void WriteJson(string jsonFilePath, Object assets) {
+                var assetsSerialized = JsonConvert.SerializeObject(assets, Formatting.Indented);
+                File.WriteAllText(jsonFilePath, assetsSerialized);
+            }
+        }
+
+        private class UnsecureLocalAssetsContainerJson
+            : AbstractLocalAssetsContainerJson
+        {
+            public static UnsecureLocalAssetsContainerJson Get(string workspacePath)
+            {
+                try
+                {
+                    string localAssetsFilePath = System.IO.Path.Combine(workspacePath, AutomationISE.Model.Constants.localAssetsFileName); 
+                    return jss.Deserialize<UnsecureLocalAssetsContainerJson>(File.ReadAllText(localAssetsFilePath));
+                }
+                catch
+                {
+                    return new UnsecureLocalAssetsContainerJson();
+                }
+            }
+
+            public static void Set(string workspacePath, LocalAssets localAssets)
+            {
+                var localAssetsUnsecure = new UnsecureLocalAssetsContainerJson();
+                foreach (var localVariableAsset in localAssets.Variables)
+                {
+                    if (!localVariableAsset.Encrypted)
+                    {
+                        localAssetsUnsecure.Variable.Add(localVariableAsset);
+                    }
+                }
+
+                WriteJson(System.IO.Path.Combine(workspacePath, AutomationISE.Model.Constants.localAssetsFileName), localAssetsUnsecure);
+            }
+           
+            //public List<CertificateJson> Certificate;
+        }
+
+        private class SecureLocalAssetsContainerJson
+            : AbstractLocalAssetsContainerJson
+        {
+            public static SecureLocalAssetsContainerJson Get(string workspacePath)
+            {
+                try
+                {
+                    string secureLocalAssetsFilePath = System.IO.Path.Combine(workspacePath, AutomationISE.Model.Constants.secureLocalAssetsFileName);
+                    return jss.Deserialize<SecureLocalAssetsContainerJson>(File.ReadAllText(secureLocalAssetsFilePath));
+                }
+                catch
+                {
+                    return new SecureLocalAssetsContainerJson();
+                }
+            }
+
+            public static void Set(string workspacePath, LocalAssets localAssets)
+            {
+                var localAssetsSecure = new SecureLocalAssetsContainerJson();
+                foreach (var localVariableAsset in localAssets.Variables)
+                {
+                    if (localVariableAsset.Encrypted)
+                    {
+                        localAssetsSecure.Variable.Add(localVariableAsset);
+                    }
+                }
+
+                localAssetsSecure.PSCredential.AddRange(localAssets.PSCredentials);
+
+                WriteJson(System.IO.Path.Combine(workspacePath, AutomationISE.Model.Constants.secureLocalAssetsFileName), localAssetsSecure); 
+            }
+
+            public List<CredentialJson> PSCredential = new List<CredentialJson>();
+            //public List<ConnectionJson> Connection;
+        }
+
+    }
+}
