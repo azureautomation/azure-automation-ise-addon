16839c484a08f6c88618dec71335bc954694baa8
Alex Stabile <alstab@microsoft.com>
2015-08-05 12:56:41 -0700

    Test Job Output Window can now retrieve the streams of the test job. It is also styled after the PS ISE's default settings.

--- AutomationISE/TestJobOutputWindow.xaml.cs
+++ AutomationISE/TestJobOutputWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
 using System.Timers;
+using System.Diagnostics;
 using Microsoft.Azure.Management.Automation.Models;
 using AutomationISE.Model;
 
@@ -33,8 +34,9 @@ namespace AutomationISE
             runbookName = name;
             jobCreateResponse = response;
             iseClient = client;
-            OutputTextBox.Text = "Job created with ID: " + jobCreateResponse.RequestId;
-            OutputTextBox.Text += "\r\nWaiting for updates...";
+            OutputTextBlock.Inlines.Add("Test job created at " + jobCreateResponse.TestJob.CreationTime);
+            OutputTextBlock.Inlines.Add("\r\nTip: not seeing Verbose output? Add the line \"$VerbosePreference='Continue'\" to your runbook.");
+            OutputTextBlock.Inlines.Add("\r\nClick 'Refresh' to check for updates.");
             //beginPolling();
         }
 
@@ -51,11 +53,59 @@ namespace AutomationISE
             TestJobGetResponse response = await iseClient.automationManagementClient.TestJobs.GetAsync(iseClient.accountResourceGroups[iseClient.currAccount].Name, 
                 iseClient.currAccount.Name, runbookName, new System.Threading.CancellationToken());
 
+            JobStreamListParameters jslParams = new JobStreamListParameters();
+
+            JobStreamListResponse jslResponse = await iseClient.automationManagementClient.JobStreams.ListTestJobStreamsAsync(iseClient.accountResourceGroups[iseClient.currAccount].Name,
+                iseClient.currAccount.Name, runbookName, null, new System.Threading.CancellationToken());
+
             this.Dispatcher.Invoke((Action)(() =>
             {
-                OutputTextBox.Text += "\r\nStatus: " + response.TestJob.Status;
-                if(response.TestJob.StatusDetails != "None")
-                    OutputTextBox.Text += "\r\n\tDetails: " + response.TestJob.StatusDetails;
+                OutputTextBlock.Inlines.Add("\r\nStatus: " + response.TestJob.Status);
+                if (response.TestJob.StatusDetails != "None")
+                    OutputTextBlock.Inlines.Add("\r\n\tDetails: " + response.TestJob.StatusDetails);
+                foreach (JobStream stream in jslResponse.JobStreams)
+                {
+                    /* Form of the 'summary' string, which is really the text we're interested in: [guid]:[hostname in brackets]:[text entered by user] */
+                    /* Strip this down to just the text entered by the user: */
+                    int secondColonIndex = stream.Properties.Summary.IndexOf(':', stream.Properties.Summary.IndexOf(':') + 1);
+                    String streamText = stream.Properties.Summary.Substring(secondColonIndex + 1);
+                    OutputTextBlock.Inlines.Add("\r\n");
+                    if (stream.Properties.StreamType == "Output")
+                    {
+                        OutputTextBlock.Inlines.Add(streamText);
+                    }
+                    else if (stream.Properties.StreamType == "Verbose")
+                    {
+                        streamText = "VERBOSE: " + streamText;
+                        OutputTextBlock.Inlines.Add(new Run(streamText) { 
+                            Foreground = (SolidColorBrush)(new BrushConverter().ConvertFrom("#FF00FFFF")),
+                            Background = (SolidColorBrush)(new BrushConverter().ConvertFrom("#00FFFFFF"))
+                        });
+                    }
+                    else if (stream.Properties.StreamType == "Error")
+                    {
+                        streamText = "ERROR: " + streamText;
+                        OutputTextBlock.Inlines.Add(new Run(streamText)
+                        {
+                            Foreground = (SolidColorBrush)(new BrushConverter().ConvertFrom("#FFFF0000")),
+                            Background = (SolidColorBrush)(new BrushConverter().ConvertFrom("#00FFFFFF"))
+                        });
+                    }
+                    else if (stream.Properties.StreamType == "Warning")
+                    {
+                        streamText = "WARNING: " + streamText;
+                        OutputTextBlock.Inlines.Add(new Run(streamText)
+                        {
+                            Foreground = (SolidColorBrush)(new BrushConverter().ConvertFrom("#FFFF8C00")),
+                            Background = (SolidColorBrush)(new BrushConverter().ConvertFrom("#00FFFFFF"))
+                        });
+                    }
+                    else
+                    {
+                        Debug.WriteLine("Unknown stream type couldn't be colored properly: " + stream.Properties.StreamType);
+                        OutputTextBlock.Inlines.Add(stream.Properties.StreamType.ToUpper() + ":  " + streamText);
+                    }
+                }
             }));
         }
         private void RefreshJobButton_Click(object sender, RoutedEventArgs e)
@@ -64,3 +114,21 @@ namespace AutomationISE
         }
     }
 }
+/*
+ * Relevant data from $ (get-host).PrivateData:
+FontName                                  : Lucida Console
+ErrorForegroundColor                      : #FFFF0000
+ErrorBackgroundColor                      : #00FFFFFF
+WarningForegroundColor                    : #FFFF8C00
+WarningBackgroundColor                    : #00FFFFFF
+VerboseForegroundColor                    : #FF00FFFF
+VerboseBackgroundColor                    : #00FFFFFF
+DebugForegroundColor                      : #FF00FFFF
+DebugBackgroundColor                      : #00FFFFFF
+ConsolePaneBackgroundColor                : #FF012456
+ConsolePaneTextBackgroundColor            : #FF012456
+ConsolePaneForegroundColor                : #FFF5F5F5
+ScriptPaneBackgroundColor                 : #FFFFFFFF
+ScriptPaneForegroundColor                 : #FF000000
+ * 
+ */
