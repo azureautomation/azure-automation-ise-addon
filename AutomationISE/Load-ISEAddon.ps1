c22467cb4ed078e42489494e1d9f54288d772f5d
eamonoreilly <eamono@microsoft.com>
2015-08-08 19:41:58 -0700

    Revert "Revert "Fixed setting correct workspace when changing certificate thumbprint""
    This reverts commit 800880be3088687782105c58ea0b4d2395e26785.

--- AutomationISE/Load-ISEAddon.ps1
+++ AutomationISE/Load-ISEAddon.ps1
@@ -1,2 +1,2 @@
-﻿Add-Type -Path .\AutomationISE.dll
+﻿Add-Type -Path C:\AutomationGit\AutomationISE\AutomationISE\bin\Debug\AutomationISE.dll
  $psISE.CurrentPowerShellTab.VerticalAddOnTools.Add(‘Azure Automation ISE Add on’, [AutomationISE.AutomationISEControl], $true)
