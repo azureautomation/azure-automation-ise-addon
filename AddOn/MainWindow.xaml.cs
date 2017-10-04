7062f0e042e9121c3e6b24bde07f04c184c3228c
eamonoreilly <eamono@microsoft.com>
2017-10-04 07:24:48 -0700

    Added separate WPF control to host add-on so it can be used with Visual Studio / VS Code on a windows environment.

---
+++ AddOn/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using AutomationISE;
+
+namespace AddOn
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var application = new AutomationISE.AutomationISEControl();
+            application.InitializeComponent();
+            this.Content = application;
+            this.Title = "Azure Automation Add-On";
+            this.Left = System.Windows.Forms.Screen.PrimaryScreen.WorkingArea.Right - this.Width;
+            this.Top = 0;
+            this.Height = System.Windows.Forms.Screen.PrimaryScreen.WorkingArea.Height;
+        }
+
+    }
+}
