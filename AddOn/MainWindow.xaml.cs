733f86c783513ba203aa8889153ed6b368fbb3c4
Eamon O'Reilly <eamono@microsoft.com>
2017-10-26 05:33:44 -0700

    Saving window position when running outside the ISE.

--- AddOn/MainWindow.xaml.cs
+++ AddOn/MainWindow.xaml.cs
@@ -12,9 +12,39 @@ namespace AddOn
             application.InitializeComponent();
             this.Content = application;
             this.Title = "Azure Automation Add-On";
-            this.Left = System.Windows.Forms.Screen.PrimaryScreen.WorkingArea.Right - this.Width;
-            this.Top = 0;
-            this.Height = System.Windows.Forms.Screen.PrimaryScreen.WorkingArea.Height;
+            if (Properties.Settings.Default.Width != -1)
+            {
+                this.Top = Properties.Settings.Default.Top;
+                this.Left = Properties.Settings.Default.Left;
+                this.Height = Properties.Settings.Default.Height;
+                this.Width = Properties.Settings.Default.Width;
+                if (Properties.Settings.Default.Maximized)
+                {
+                    WindowState = WindowState.Maximized;
+                }
+            }
+            this.Closing += MainWindow_Closing;
+        }
+
+        private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (WindowState == WindowState.Maximized)
+            {
+                Properties.Settings.Default.Top = RestoreBounds.Top;
+                Properties.Settings.Default.Left = RestoreBounds.Left;
+                Properties.Settings.Default.Height = RestoreBounds.Height;
+                Properties.Settings.Default.Width = RestoreBounds.Width;
+                Properties.Settings.Default.Maximized = true;
+            }
+            else
+            {
+                Properties.Settings.Default.Top = this.Top;
+                Properties.Settings.Default.Left = this.Left;
+                Properties.Settings.Default.Height = this.Height;
+                Properties.Settings.Default.Width = this.Width;
+                Properties.Settings.Default.Maximized = false;
+            }
+            Properties.Settings.Default.Save();
         }
 
     }
